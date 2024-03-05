98c031fbe4a875046fb2a86ae5787f7860726b3b
Abdualeem Hosny <abdualeem.hosny@nagwa.com>
2024-03-05 16:44:05 +0200

    Add project files.

---
+++ IdealWeightCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IdealWeightCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          WeightCalculator weightCalculator = new WeightCalculator() { Height =180 ,Sexe='m'};
+            var weight=weightCalculator.GetIdealBodyWeight();
+
+            Console.WriteLine(  $" The Ideal Weight Body is : {weight}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IdealWeightCalculator/WeightCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace IdealWeightCalculator
+{
+    public class WeightCalculator
+    {
+        public double Height { get; set; }
+        public char Sexe { get; set; }
+
+        public double GetIdealBodyWeight()
+        {
+            switch(Sexe)
+            {
+                case 'm':
+                    return (Height -100) - ((Height - 150) /4);
+                case 'w':
+                    return (Height - 100) - ((Height - 150) / 4);
+                default: return 0;
+            }
+        }
+    }
+}
